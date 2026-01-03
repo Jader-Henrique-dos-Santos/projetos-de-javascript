a5d28e6f123cb6d952d25b6b68d0fac705a12820
Jader Henrique dos Santos <jh5515785@gmail.com>
2026-01-03 18:54:38 -0300

    arrumando codigos
    do index(2).html e script(2).js

--- script(2).js
+++ script(2).js
@@ -1,18 +1,11 @@
-// Variáveis iniciais (vazias)
-let primeiro_valor = ''
-let segundo_valor = ''
-
-// Inputs
 let inputvalor1 = document.getElementById('valor-1')
 let inputvalor2 = document.getElementById('valor-2')
 let botao = document.getElementById('calcular')
 let resultado = document.getElementById('resultado')
 
-// Ao clicar no botão
 botao.addEventListener('click', function() {
-    primeiro_valor = Number(inputvalor1.value)
-    segundo_valor = Number(inputvalor2.value)
-
+    let primeiro_valor = Number(inputvalor1.value)
+    let segundo_valor = Number(inputvalor2.value)
     let soma = primeiro_valor + segundo_valor
     resultado.value = soma
 })
